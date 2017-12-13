d6432b8f37aec02fde2f2ac33af99d98980c7453
Saed Moradi <moradisaed@users.noreply.github.com>
2017-12-14 02:51:25 +0330

    Update localmean.m

--- localmean.m
+++ localmean.m
@@ -3,7 +3,7 @@ function mean=localmean(f,nhood)
 % performing local averaging on 2D images
 % inputs: f => input images
 %	  nhood => the neighbourhood matrix
-%		    e.g. ones(5)
+%		      e.g. ones(5)
 % output: mean => local average matrix	
 
 if nargin==1
