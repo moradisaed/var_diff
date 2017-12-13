ef8e642a2b935dbcffee5b107aedf308e583d1e3
Saed Moradi <moradisaed@users.noreply.github.com>
2017-12-14 02:50:54 +0330

    Update pre_proc.m

--- pre_proc.m
+++ pre_proc.m
@@ -12,17 +12,13 @@ img=imread('test.bmp');
 img1=imfilter(img,[0.5;0;0.5],'replicate');
 img2=abs(img1-img);
 Thresh_n=50;
-%figure;imshow(img2,[])
 img2=uint8(img2-Thresh_n);
 
-%figure;imshow(img2,[])
 mask=logical(img2);
 mask2=~mask;
 img3=double(mask2).*img;
-%figure;imshow(img3,[])
 
 img4=double(mask).*img1;
-%figure;imshow(img4,[])
 
 out=img3+img4;
 figure;imshow(out,[])
